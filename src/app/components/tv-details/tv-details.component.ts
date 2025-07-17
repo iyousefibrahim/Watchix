17ccd54995ffa01e328fc65f5ed01e5f6fef440c
Yousef <yousefibrahim9@outlook.com>
2025-07-17 19:19:56 +0300

    style: improve skeleton card layout for visual consistency

--- src/app/components/tv-details/tv-details.component.ts
+++ src/app/components/tv-details/tv-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { TVService } from '../../core/services/tv.service';
+import { TranslationService } from '../../core/services/translation.service';
+import { TVDetails } from '../../core/interfaces/responses/tv-details';
+import { imagePath } from '../../shared/utils/imagePath';
 
 @Component({
   selector: 'app-tv-details',
@@ -7,6 +11,44 @@ import { Component, input } from '@angular/core';
   styleUrl: './tv-details.component.css'
 })
 export class TvDetailsComponent {
-  Id = input.required<string | null>();
+  private readonly _TVService = inject(TVService);
+  private readonly _TranslationService = inject(TranslationService);
 
+  Id = input.required<string | null>();
+  tvDetails = signal<TVDetails | null>(null);
+  imagePath = imagePath;
+
+  ngOnInit(): void {
+    this.getTVDetails();
+    this._TranslationService.languageChanged$.subscribe({
+      next: () => {
+        this.getTVDetails();
+      }
+    });
+  }
+
+  private getTVDetails(): void {
+    if (this.Id()) {
+      this._TVService.getTVDetails(Number(this.Id())).subscribe({
+        next: (response) => {
+          this.tvDetails.set(response);
+        },
+        error: (error) => {
+          console.error('Error fetching movie details:', error);
+        }
+      });
+    }
+  }
+
+  // private getTVEpisodes(): void {
+  //   if (this.Id()) {
+  //     this._TVService.getEpisodeDetails().subscribe({
+  //       next: (response) => {
+  //         // Handle the response for TV episodes if needed
+  //       },
+  //       error: (error) => {
+  //         console.error('Error fetching TV episodes:', error);
+  //       }
+  //     });
+  //   }
 }
