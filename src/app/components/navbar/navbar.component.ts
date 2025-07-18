638a24bc4b569ef1bf17f6a7bd446480628d8ade
Yousef <yousefibrahim9@outlook.com>
2025-07-18 16:09:41 +0300

    feat(navbar): enhance theme handling and dropdown functionality with signals

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,35 +1,48 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../core/services/translation.service';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [TranslateModule, RouterLink],
+  imports: [TranslateModule, RouterLink, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  dropdownOpen = false;
-
   constructor(private _TranslationService: TranslationService) { }
 
+  currentTheme = signal<'nord' | 'black'>('nord');
+  dropdownOpen = signal(false);
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme') as 'nord' | 'black' | null;
+    const themeToUse = savedTheme ?? 'nord';
+
+    document.documentElement.setAttribute('data-theme', themeToUse);
+    this.currentTheme.set(themeToUse);
+  }
+
+
   toggleDropdown() {
-    this.dropdownOpen = !this.dropdownOpen;
+    this.dropdownOpen.update(open => !open);
   }
 
   selectLang(lang: string) {
     this._TranslationService.changeLanguage(lang);
-    this.dropdownOpen = false
+    this.dropdownOpen.set(false);
   }
 
   changeMode(): void {
-    const html = document.querySelector('html');
-    if (!html) return;
+    const html = document.documentElement;
+    const current = html.getAttribute('data-theme');
+    const newTheme = current === 'nord' ? 'black' : 'nord';
 
-    const currentTheme = html.getAttribute('data-theme');
-    const newTheme = currentTheme === 'nord' ? 'black' : 'nord';
     html.setAttribute('data-theme', newTheme);
+    this.currentTheme.set(newTheme as 'nord' | 'black');
+    localStorage.setItem('theme', newTheme);
   }
+
 }
